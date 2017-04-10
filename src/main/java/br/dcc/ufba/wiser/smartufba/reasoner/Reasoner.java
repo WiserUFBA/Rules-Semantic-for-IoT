855a949573a42e20dbd6956c2c66e6899c211065
gustavoramos00 <gustavoramos00@gmail.com>
2017-04-10 18:46:36 -0300

    Criando regra para atualizar modelo

--- src/main/java/br/dcc/ufba/wiser/smartufba/reasoner/Reasoner.java
+++ src/main/java/br/dcc/ufba/wiser/smartufba/reasoner/Reasoner.java
@@ -3,15 +3,13 @@ package br.dcc.ufba.wiser.smartufba.reasoner;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Properties;
 
-import org.apache.jena.rdf.model.InfModel;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.rdf.model.*;
 import org.apache.jena.reasoner.Derivation;
 import org.apache.jena.reasoner.rulesys.GenericRuleReasoner;
 import org.apache.jena.reasoner.rulesys.Rule;
@@ -20,9 +18,9 @@ import org.apache.jena.util.FileManager;
 
 public class Reasoner {
 
-	// private static String fname = "http://192.168.0.109:3030/sistemasweb/";
-	 private static String NS = "@prefix j.0: <http://www.loa-cnr.it/ontologies/DUL.owl#>";
-	private static String fname = "";
+	 private static String fname = "http://192.168.0.109:3030/sistemasweb/";
+//	private static String fname = "teste.ttl";
+	private String updateTripleStore = "updateTripleStore.txt";
 	 
 	 static {
 	 try{
@@ -32,76 +30,53 @@ public class Reasoner {
 	 }
 	 catch(IOException iox)
 	 {
-		 System.out.println("Arquivo não localizado");
+		 System.out.println("Arquivo nÃ£o localizado");
 	 }
-	} 	
-	 
-	 
-	 public void reasoner(){
+	}
+
+
+    public void reasoner() throws IOException, URISyntaxException {
 			
 		    Model data = FileManager.get().loadModel(fname );
-		    
-		
-		    String rules = "[rule1: (?a j.0:hasDataValue ?b) (?b j.0:p ?c) -> (?a j.0:p ?c)]";
+
+//		    String rules = "[rule1: (?a j.0:hasDataValue ?b) (?b j.0:p ?c) -> (?a j.0:p ?c)]";
 		   
-		    /*  Regra sendo elaborada pelo Gustavo
+//		      Regra sendo elaborada pelo Gustavo
 			   
-		   // List<Rule> regras = Rule.parseRules(rules);
+//		    List<Rule> regras = Rule.parseRules(rules);
 		    
-		    GenericRuleReasoner reasoner = new GenericRuleReasoner(Rule.rulesFromURL("C:\\Users\\Cleber\\workspace\\semantic-reasoner\\src\\main\\resources\\rules.txt"));
+		    GenericRuleReasoner reasoner = new GenericRuleReasoner(Rule.rulesFromURL("rules.txt"));
 		    reasoner.setDerivationLogging(true);
 		    InfModel inf = ModelFactory.createInfModel(reasoner, data);
 		    
-		    
 		    PrintWriter out = new PrintWriter(System.out);
 		    
-		    
-		    for (StmtIterator i = inf.listStatements(inf.getResource(NS+"A"), 	inf.getProperty(NS+"p"), inf.getResource(NS+"D")); i.hasNext(); ) {
-		        Statement s = i.nextStatement(); 
-		        System.out.println("Statement is " + s);
+//         for (StmtIterator i = inf.listStatements(null, inf.getProperty("highTemperature"), null, data); i.hasNext(); ) {
+         for (StmtIterator i = inf.listStatements(); i.hasNext(); ) {
+		        Statement s = i.nextStatement();
 		        for (Iterator id = inf.getDerivation(s); id.hasNext(); ) {
+                    System.out.println("Statement is " + s);
 		            Derivation deriv = (Derivation) id.next();
-		            deriv.printTrace(out, true);        
-		          }     
+		            deriv.printTrace(out, true);
+                    RDFNode object = s.getObject();
+                    System.out.println("Object is " + object.toString());
+                    updateModel(data, s.getSubject());
+                }
 		    }
-		    out.flush(); 	
-		    
-		    */
-		  
-		    UpdateModel updatemodel = new UpdateModel();
-		    
-		    //Caso ocorra o Match na inferência o modelo é atualizado  ()
-		   
-		    String tripleStoreURI = "" +
-	                "PREFIX  j.1: <http://purl.oclc.org/NET/ssnx/ssn#>\n" +
-	                "PREFIX  j.0: <http://www.loa-cnr.it/ontologies/DUL.owl#>\n" +
-	                "PREFIX xsd:   <http://www.w3.org/2001/XMLSchema#>\n" +
-	               
-	                "DELETE { <http://wiser.dcc.ufba.br/smartUFBA/devices/ufbaino#obsValue_14915308050001491530865086>\n" + 
-          "						       a                 j.1:ObservationValue ;\n"  + 
-          "                            j.0:hasDataValue  \"37\"^^xsd:double ;\n" +
-          "                            j.0:isSettingFor  false .}\n" +
-           
-          "INSERT { <http://wiser.dcc.ufba.br/smartUFBA/devices/ufbaino#obsValue_14915308050001491530865086>\n" +
-          "                  a                 j.1:ObservationValue ;\n" +
-          "                  j.0:hasDataValue  \"37\"^^xsd:double ;\n" +
-          "                  j.0:isSettingFor  true .}\n " +
-          
-  "  WHERE { <http://wiser.dcc.ufba.br/smartUFBA/devices/ufbaino#obsValue_14915308050001491530865086>" +
-  "                   a                 j.1:ObservationValue ;\n" +
-  "                   j.0:hasDataValue  \"37\"^^xsd:double ;\n" +
-  "                   j.0:isSettingFor  false . }";
-   
-		
-		 
-		  
-	 	  updatemodel.updateTripleStore(tripleStoreURI, data,fname);
-		    
-		    
-
+		    out.flush();
 		}
 
-	 public static Properties getProp() throws IOException {
+    private void updateModel(Model data, Resource subject) throws IOException, URISyntaxException {
+        UpdateModel updatemodel = new UpdateModel();
+        //Caso ocorra o Match na inferÃªncia o modelo Ã© atualizado  ()
+        StringBuilder sb = new StringBuilder();
+        Files.lines(Paths.get(ClassLoader.getSystemResource(updateTripleStore).toURI()))
+            .forEach(line -> sb.append(line + "\n"));
+        String tripleStoreURI = String.format(sb.toString(), subject.toString(), subject.toString(), subject.toString());
+        updatemodel.updateTripleStore(tripleStoreURI, data,fname);
+    }
+
+    public static Properties getProp() throws IOException {
 			Properties props = new Properties();
 			FileInputStream file = new FileInputStream("./main/resources/br.ufba.dcc.wiser.smartufba.semanticrules.properties");
 			props.load(file);
@@ -109,7 +84,7 @@ public class Reasoner {
 		}
 
 	 
-	 public static void main (String [] args){
+	 public static void main (String [] args) throws IOException, URISyntaxException {
 		 Reasoner reasoner = new Reasoner();
 		 reasoner.reasoner();
 	 }
--- src/main/java/br/dcc/ufba/wiser/smartufba/reasoner/UpdateModel.java
+++ src/main/java/br/dcc/ufba/wiser/smartufba/reasoner/UpdateModel.java
@@ -1,19 +1,22 @@
 package br.dcc.ufba.wiser.smartufba.reasoner;
 
+import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetAccessor;
 import org.apache.jena.query.DatasetAccessorFactory;
+import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.update.UpdateAction;
 
 public class UpdateModel {
 
-	
-		
-	public synchronized void updateTripleStore(String tripleStoreURI, Model model, String adressModel) {
-		
-		UpdateAction.parseExecute(tripleStoreURI, model );
-		DatasetAccessor accessor = DatasetAccessorFactory.createHTTP(adressModel);
-		accessor.putModel(model);
+    public synchronized void updateTripleStore(String tripleStoreURI, Model model, String adressModel) {
 
-	}
+        UpdateAction.parseExecute(tripleStoreURI, model);
+        Dataset ds = DatasetFactory.create();
+        DatasetAccessor accessor = DatasetAccessorFactory.createHTTP(adressModel);
+//        DatasetAccessor accessor = DatasetAccessorFactory.create(ds);
+//        model.write(System.out, "TTL");
+        accessor.putModel(model);
+
+    }
 }
